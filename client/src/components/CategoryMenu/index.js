7ad58308fd80af9ad4701be396b5001a9ccdcece
odupree1988 <odupree1988@gmail.com>
2021-09-16 13:23:42 -0400

    created cart ui

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,11 +1,8 @@
-import React, { useEffect } from "react";
-import { useQuery } from "@apollo/client";
-import {
-  UPDATE_CATEGORIES,
-  UPDATE_CURRENT_CATEGORY,
-} from "../../utils/actions";
-import { QUERY_CATEGORIES } from "../../utils/queries";
-import { useStoreContext } from "../../utils/GlobalState";
+import React, { useEffect } from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { useStoreContext } from '../../utils/GlobalState';
+import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
+import { QUERY_CATEGORIES } from '../../utils/queries';
 
 function CategoryMenu() {
   const [state, dispatch] = useStoreContext();
@@ -15,27 +12,25 @@ function CategoryMenu() {
   const { data: categoryData } = useQuery(QUERY_CATEGORIES);
 
   useEffect(() => {
-    // if categoryData exists or has changed from the response of useQuery, then run dispatch()
     if (categoryData) {
-      // execute our dispatch function with our action object indicating the type of action and the data to set our state for categories
       dispatch({
         type: UPDATE_CATEGORIES,
-        categories: categoryData.categories,
+        categories: categoryData.categories
       });
     }
   }, [categoryData, dispatch]);
 
-  const handleClick = (id) => {
+  const handleClick = id => {
     dispatch({
       type: UPDATE_CURRENT_CATEGORY,
-      currentCategory: id,
+      currentCategory: id
     });
   };
 
   return (
     <div>
       <h2>Choose a Category:</h2>
-      {categories.map((item) => (
+      {categories.map(item => (
         <button
           key={item._id}
           onClick={() => {
--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
-import { useQuery } from "@apollo/client";
+import { useQuery } from '@apollo/react-hooks';
 
 import ProductItem from "../ProductItem";
-import { QUERY_PRODUCTS } from "../../utils/queries";
 import { useStoreContext } from "../../utils/GlobalState";
 import { UPDATE_PRODUCTS } from "../../utils/actions";
-import spinner from "../../assets/spinner.gif";
+import { QUERY_PRODUCTS } from "../../utils/queries";
+import spinner from "../../assets/spinner.gif"
 
 function ProductList() {
   const [state, dispatch] = useStoreContext();
@@ -15,22 +15,20 @@ function ProductList() {
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   useEffect(() => {
-    if (data) {
+    if(data) {
       dispatch({
-        type: UPDATE_PRODUCTS,
-        products: data.products,
-      });
+           type: UPDATE_PRODUCTS,
+          products: data.products
+        });
     }
   }, [data, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
-      return state.products.filter(
-        (product) => product.category._id === currentCategory
-      );
+      return state.products;
     }
 
-    return state.products;
+    return state.products.filter(product => product.category._id === currentCategory);
   }
 
   return (
@@ -38,21 +36,22 @@ function ProductList() {
       <h2>Our Products:</h2>
       {state.products.length ? (
         <div className="flex-row">
-          {filterProducts().map((product) => (
-            <ProductItem
-              key={product._id}
-              _id={product._id}
-              image={product.image}
-              name={product.name}
-              price={product.price}
-              quantity={product.quantity}
-            />
-          ))}
+            {filterProducts().map(product => (
+                <ProductItem
+                  key= {product._id}
+                  _id={product._id}
+                  image={product.image}
+                  name={product.name}
+                  price={product.price}
+                  quantity={product.quantity}
+                />
+            ))}
         </div>
       ) : (
         <h3>You haven't added any products yet!</h3>
       )}
-      {loading ? <img src={spinner} alt="loading" /> : null}
+      { loading ? 
+      <img src={spinner} alt="loading" />: null}
     </div>
   );
 }
