f4e1cfd67f62e2a63f7970a2901f5ca4f3df2f0b
odupree1988 <odupree1988@gmail.com>
2021-09-17 19:38:11 -0400

    heroku

---
+++ client/build/precache-manifest.dc015c828e3ad6673a1d152c5aa726c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe7907837aa4221a473ee6981b31e4c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39135748ab032ec45b59",
+    "url": "/static/css/main.e213c893.chunk.css"
+  },
+  {
+    "revision": "7acfbf5d72f712b5e250",
+    "url": "/static/js/2.79c6e279.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.79c6e279.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39135748ab032ec45b59",
+    "url": "/static/js/main.ab3566a8.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
